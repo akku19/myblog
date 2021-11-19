c0ce8d23dd819db8e3ec8151b241137d222f7dc9
akku19 <akashsen127@gmail.com>
2021-11-19 18:44:57 +0530

    Commit message.

--- src/About.js
+++ src/About.js
@@ -3,11 +3,9 @@ import React from 'react'
 const About = () => {
     return (
         <div>
-<<<<<<< HEAD
+
             About page
-=======
-            About
->>>>>>> 9ce9ee7bab7b6b050529b7d41f4d112134ab6a12
+
         </div>
     )
 }
--- src/App.js
+++ src/App.js
@@ -5,40 +5,20 @@ import About from './About';
 import Contact from './Contact';
 import NotFound from './NotFound'
 import Header from './Header';
-<<<<<<< HEAD
-import Content  from './Content';
-=======
->>>>>>> 9ce9ee7bab7b6b050529b7d41f4d112134ab6a12
+import Content from './Content'
 
 const App = () => {
   return (
     <>
-<<<<<<< HEAD
- 
+    <Header />
     <div className="contents">
      <Routes>
         <Route path="/" exact element={<Content />} /> 
         <Route path="/about" exact   element={<About />} /> 
-=======
-    <Header />
-    <div className="contents">
-     <Routes>
-        <Route path="/" exact element={<About />} /> 
-        <Route path="/about" exact   element={<Contact />} /> 
->>>>>>> 9ce9ee7bab7b6b050529b7d41f4d112134ab6a12
         <Route path="*" element={<NotFound/>}/>
        </Routes>
      </div>
     </>
   )
 }
-<<<<<<< HEAD
 export default App ;
-=======
-
-App.propTypes = {
-
-}
-
-export default App
->>>>>>> 9ce9ee7bab7b6b050529b7d41f4d112134ab6a12
